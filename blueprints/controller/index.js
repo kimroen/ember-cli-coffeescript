948032d6ffdc549ee31a5452c7aa51757ba5c676
Kim Røen <kim@kimroen.com>
2014-12-01 11:46:23 +0100

    Remove types for generated controllers

--- blueprints/controller/index.js
+++ blueprints/controller/index.js
@@ -1,14 +1,3 @@
 module.exports = {
-  description: 'Generates a controller of the given type.',
-
-  availableOptions: [
-    { name: 'type', values: ['basic', 'object', 'array'], default: 'basic' }
-  ],
-
-  beforeInstall: function(options) {
-    return require('ember-cli/blueprints/controller').beforeInstall.call(this, options);
-  },
-  locals: function(options) {
-    return this.lookupBlueprint('controller').locals(options);
-  }
+  description: 'Generates a controller.'
 };
