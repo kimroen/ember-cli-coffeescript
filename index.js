f8d303088489afec1e7020dfa93f4283b90691b8
Kim Røen <kim@kimroen.com>
2014-12-08 00:41:24 +0100

    Redo addon for simpler object syntax

--- index.js
+++ index.js
@@ -2,25 +2,18 @@
 var path = require('path');
 var CoffeePreprocessor = require('./lib/coffee-preprocessor');
 
-function CoffeescriptAddon(project) {
-  this._project = project;
-  this.name     = 'Ember CLI Coffeescript Addon';
-}
+module.exports = {
+  name: 'Ember CLI Coffeescript Addon',
 
-CoffeescriptAddon.prototype.blueprintsPath = function() {
-  return path.join(__dirname, 'blueprints');
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  },
+
+  included: function(app) {
+    this.app = app;
+
+    var plugin = new CoffeePreprocessor(this.app.options.coffeeOptions);
+
+    this.app.registry.add('js', plugin);
+  }
 };
-
-CoffeescriptAddon.prototype.included = function(app) {
-  this.app = app;
-
-  var plugin = new CoffeePreprocessor(this.app.options.coffeeOptions);
-
-  this.app.registry.add('js', plugin);
-};
-
-// This is just here because it was required in ember-cli v0.0.37.
-// To be removed when compatibility breaks.
-CoffeescriptAddon.prototype.treeFor = function() {};
-
-module.exports = CoffeescriptAddon;
