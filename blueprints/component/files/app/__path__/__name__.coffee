ddd4da4a1d12b2e152ef2afae890725586c989c3
Kim Røen <kim@kimroen.com>
2015-03-10 22:04:43 +0100

    Redo components to match ember-cli

--- blueprints/component/files/app/__path__/__name__.coffee
+++
@@ -1,5 +0,0 @@
-`import Ember from 'ember'`
-
-<%= classifiedModuleName %>Component = Ember.Component.extend()
-
-`export default <%= classifiedModuleName %>Component`
--- blueprints/component/index.js
+++ blueprints/component/index.js
@@ -1,12 +1,36 @@
 module.exports = {
   description: 'Generates a component. Name must contain a hyphen.',
 
-  normalizeEntityName: function(entityName) {
-    entityName = this.lookupBlueprint('component').normalizeEntityName(entityName);
-    return entityName;
-  },
-
   fileMapTokens: function() {
     return this.lookupBlueprint('component').fileMapTokens();
+  },
+
+  normalizeEntityName: function(entityName) {
+    return this.lookupBlueprint('component').normalizeEntityName(entityName);
+  },
+
+  locals: function(options) {
+    var locals = this.lookupBlueprint('component').locals(options);
+
+    // This is here to work around this bug in ember-cli:
+    // https://github.com/ember-cli/ember-cli/issues/4001
+    if (!this.project) {
+      return locals;
+    }
+
+    var newContents = '';
+    if (locals.contents) {
+      newContents = locals.contents + '\n';
+    }
+
+    var newImport = '';
+    if (locals.importTemplate) {
+      newImport = "`" + locals.importTemplate.replace(/;/, '`');
+    }
+
+    locals.contents = newContents;
+    locals.importTemplate = newImport;
+
+    return locals;
   }
 };
