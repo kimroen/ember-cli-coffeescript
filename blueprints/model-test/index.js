bfa055f802595ef473f2761f7b434e1d170cdb4d
Kim Røen <kim@kimroen.com>
2015-08-06 01:37:07 +0200

    Update model-test blueprint

--- blueprints/model-test/index.js
+++ blueprints/model-test/index.js
@@ -1,3 +1,8 @@
 module.exports = {
-  description: 'Generates a model unit test.'
+  description: 'Generates a model unit test.',
+
+  locals: function() {
+    var blueprint = this.lookupBlueprint('model-test');
+    return blueprint.locals.apply(blueprint, arguments);
+  }
 };
