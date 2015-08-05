608259ab57cc8a571840bc0ea7dc68118350adaf
Kim Røen <kim@kimroen.com>
2015-08-06 00:48:12 +0200

    Update adapter blueprint

--- blueprints/adapter/index.js
+++ blueprints/adapter/index.js
@@ -2,11 +2,11 @@ module.exports = {
   description: 'Generates an ember-data adapter.',
 
   availableOptions: [
-    { name: 'base-class', type: String, default: 'application' }
+    { name: 'base-class', type: String }
   ],
-  locals: function(options) {
-
-    var locals = this.lookupBlueprint('adapter').locals(options);
+  locals: function() {
+    var blueprint = this.lookupBlueprint('adapter');
+    var locals = blueprint.locals.apply(blueprint, arguments);
     locals.importStatement = locals.importStatement.replace(/;$/, '');
     return locals;
   }
