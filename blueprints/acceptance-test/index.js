d493445e497ce34358ee14e5d8b2c3489149acaf
Kim Røen <kim@kimroen.com>
2015-08-06 00:47:41 +0200

    Update acceptance-test blueprint

--- blueprints/acceptance-test/index.js
+++ blueprints/acceptance-test/index.js
@@ -1,3 +1,7 @@
 module.exports = {
-  description: 'Generates an acceptance test for a feature.'
+  description: 'Generates an acceptance test for a feature.',
+  locals: function() {
+    var blueprint = this.lookupBlueprint('acceptance-test');
+    return blueprint.locals.apply(blueprint, arguments);
+  }
 };
