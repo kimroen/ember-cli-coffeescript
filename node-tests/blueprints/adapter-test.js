4b6398225e2581883172603c52fdda4e94a25d77
Kim Røen <kim@kimroen.com>
2016-12-11 13:05:55 +0100

    Add inital test for adapter blueprint

---
+++ node-tests/blueprints/adapter-test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
+var setupTestHooks = blueprintHelpers.setupTestHooks;
+var emberNew = blueprintHelpers.emberNew;
+var emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
+
+var expect = require('ember-cli-blueprint-test-helpers/chai').expect;
+
+describe('Acceptance: ember generate and destroy adapter', function() {
+  setupTestHooks(this);
+
+  it('adapter foo', function() {
+    var args = ['adapter', 'foo'];
+
+    return emberNew()
+      .then(() => emberGenerateDestroy(args, (file) => {
+        expect(file('app/adapters/foo.coffee'))
+          .to.contain("`import ApplicationAdapter from './application'`")
+          .to.contain("FooAdapter = ApplicationAdapter.extend()")
+          .to.contain("`export default FooAdapter`");
+
+        expect(file('tests/unit/adapters/foo-test.coffee'))
+          .to.contain("moduleFor 'adapter:foo'")
+    }));
+  });
+});
